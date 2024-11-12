3d1d38ee23b51034634a571c55a69dbf41b8683f
Bodu_wk69 <Bodu_wk69@student.itstep.org>
2024-11-12 23:43:43 +0100

    bugFix6

--- application/controllers/api/Orders.php
+++ application/controllers/api/Orders.php
@@ -13,7 +13,7 @@ class Orders extends CI_Controller {
             'user_phone' => $this->input->post('user_phone'),
             'user_id' => $u_id,
             'order_time' => date('Y-m-d H:i:s'),
-            'total_prise' => $this->calculate_total_price($u_id),
+            'total_prise' => $this->Orders_model->calculate_total_price($u_id),
         );
         $order_id = $this->Orders_model->create_order($data);
         echo json_encode(array("data" => $data, 'Orders created successfuly'));
@@ -45,9 +45,9 @@ class Orders extends CI_Controller {
         $cart_items = $this->Orders_model->get_cart_items_by_user_id($user_id);
         echo json_encode($cart_items);
     }
-    public function calculate_total_price($user_id) {
-        $cart_items = $this->Orders_model->get_cart_items_by_user_id($user_id);
-        $total_price = $this->Orders_model->calculate_total_price($cart_items);
-        echo json_encode(array('total_price' => $total_price));
-    }
+    //public function calculate_total_price($user_id) {
+    //    $cart_items = $this->Orders_model->get_cart_items_by_user_id($user_id);
+    //    $total_price = $this->Orders_model->calculate_total_price($cart_items);
+    //    echo json_encode(array('total_price' => $total_price));
+    //}
 }
